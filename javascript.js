fec12e04e2f5249a2ab49946d6f79d8a31008f27
andreasgeorgiou <ageorgiou4@uclan.ac.uk>
2019-11-23 14:25:27 +0200

    added in js

--- javascript.js
+++ javascript.js
@@ -11,12 +11,21 @@ function getChallenges () {
             let challengesList = document.getElementById("challenges");
             let treasureHuntsArray = jsonObject.treasureHunts;
 
-            for (let i=0; i<treasureHuntsArray.length; i++) {
+            for (let i = 0; i < treasureHuntsArray.length; i++) {
                 let listItem = document.createElement("li");
                 let uuid = treasureHuntsArray[i].uuid;
-                listItem.innerHTML ="<a href='https://codecyprus.org/th/api/start?player=" + playerName + "&app=" + appName + "&treasure-hunt-id=" + uuid +"'>" + treasureHuntsArray[i].name + "</a>";
+                listItem.innerHTML = "<a href='https://codecyprus.org/th/api/start?player=" + playerName + "&app=" + appName + "&treasure-hunt-id=" + uuid + "'>" + treasureHuntsArray[i].name + "</a>";
                 challengesList.appendChild(listItem);
 
             }
+        });
+}
+
+function getQuestions() {
+    fetch("https://codecyprus.org/th/api/question?session=ag9nfmNvZGVjeXBydXNvcmdyFAsSB1Nlc3Npb24YgICAoMa0gQoM")
+        .then(response => response.json()) //Parse JSON text to JavaScript object
+        .then(jsonObject => {
+            console.log(jsonObject);
+
         });
 }
