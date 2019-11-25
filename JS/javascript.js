64c65df86712767c50c6dec5615f290ca91fe18e
Andreas Georgiou <ageorgiou4@uclan.ac.uk>
2019-11-25 15:12:30 +0200

    Update javascript.js

--- JS/javascript.js
+++ JS/javascript.js
@@ -87,7 +87,7 @@ function QRCodeReader() {
 
     Instascan.Camera.getCameras().then(function (cameras) {
         if (cameras.length > 0) {
-            scanner.start(cameras[1]);
+            scanner.start(cameras[0]);
         } else {
             console.error('No cameras found.');
             alert("No cameras found.");
