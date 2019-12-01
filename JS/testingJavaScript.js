2d365334e350e0790fcec87fbddedbf9ea7d3a19
Iakovos Kyriakou <IKyriakou@uclan.ac.uk>
2019-12-01 11:39:53 +0200

    testing

--- JS/testingJavaScript.js
+++ JS/testingJavaScript.js
@@ -2,20 +2,20 @@
 
 const TESTING_API_LIST = "https://codecyprus.org/th/test-api/list?number-of-ths=2";
 
-
 function getTestingList(){
 fetch(TESTING_API_LIST)
     .then(response =>response.json()) //ParseJSON text to JavaScript object
     .then(jsonObject => {
         console.log(jsonObject); //TODO - Success, do something with the data.
-        let array = jsonObject.treasureHunts;
 
-        let uuid = array[i].uuid;
+
+
+
 
         for(let i=0; i<array.length; i++){
-
+            let uuid = array[i].uuid;
             let listItem = document.createElement("li");
-
+            let array = jsonObject.treasureHunts;
             listItem.innerHTML =  "<a href='https://codecyprus.org/th/test-api/start?player=Jacobs&app=Team2&treasure-hunt-id="+uuid+"'>" + array[i].name + "</a>";
 
             list.appendChild(listItem);
