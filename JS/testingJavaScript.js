954bfc526e10e5fc01e8652ef698769b1c53bb02
Kostas Ioannou <Kioannou2@uclan.ac.uk>
2019-12-01 12:50:56 +0200

    Testing /list and /start prototypes

--- JS/testingJavaScript.js
+++ JS/testingJavaScript.js
@@ -1,25 +1,50 @@
 //JAVASCRIPT FOR TESTING
 
 const TESTING_API_LIST = "https://codecyprus.org/th/test-api/list?number-of-ths=2";
-
-function getTestingList(){
-fetch(TESTING_API_LIST)
-    .then(response =>response.json()) //ParseJSON text to JavaScript object
-    .then(jsonObject => {
-        console.log(jsonObject); //TODO - Success, do something with the data.
+const TESTING_API_START = "https://codecyprus.org/th/test-api/start?player=inactive";
 
 
 
+let list = document.getElementById("challenges");
+function getChallengesTest() {
+    fetch("https://codecyprus.org/th/test-api/list?number-of-ths=2")
+        .then(response => response.json()) //Parse JSON text to JavaScript object
+        .then(jsonObject => {
+            console.log(jsonObject);
 
-
-        for(let i=0; i<array.length; i++){
-            let uuid = array[i].uuid;
-            let listItem = document.createElement("li");
             let array = jsonObject.treasureHunts;
-            listItem.innerHTML =  "<a href='https://codecyprus.org/th/test-api/start?player=Jacobs&app=Team2&treasure-hunt-id="+uuid+"'>" + array[i].name + "</a>";
 
-            list.appendChild(listItem);
-        }
-        console.log(array);
-    });
+            for (let i = 0; i < array.length; i++) {
+                list = document.getElementById("challenges");
+                let uuid = array[i].uuid;
+                let listItem = document.createElement("li");
+
+                listItem.innerHTML = "<a href='https://codecyprus.org/th/test-api/start?player=Jacobs&app=Team2&treasure-hunt-id=" + uuid + "'>" + array[i].description + "</a>";
+
+                list.appendChild(listItem);
+            }
+
+        });
+
+}
+
+    function getStartTesting(uuid){
+    fetch("https://codecyprus.org/th/test-api/start?player=Kostas&app=Team2&treasure-hunt-id=" + uuid)
+        .then(response => response.json())
+        .then(jsonObject => {
+            console.log(jsonObject);
+
+            let array = jsonObject.errorMessages;
+
+            for (let i = 0; i<array.length; i++){
+                let error = array[i].status;
+                let list = document.getElementById("errors");
+                let errorList = document.createElement("li");
+                errorList.innerHTML = "<a href='https://codecyprus.org/th/test-api/start?player=Jacobs&app=Team2&treasure-hunt-id=" + uuid + "'>" + array[i].name + "</a>";
+                list.appendChild(errorList);
+            }
+            if (status === "ERROR"){
+                alert(jsonObject.errorMessages);
+            }
+        });
 }
