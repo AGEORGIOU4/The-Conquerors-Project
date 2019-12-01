6923f444a19a61b6899d29b4c1e4ba09e856241f
Iakovos Kyriakou <IKyriakou@uclan.ac.uk>
2019-12-01 11:11:37 +0200

    testing

--- JS/testingJavaScript.js
+++ JS/testingJavaScript.js
@@ -8,6 +8,20 @@ fetch(TESTING_API_LIST)
     .then(response =>response.json()) //ParseJSON text to JavaScript object
     .then(jsonObject => {
         console.log(jsonObject); //TODO - Success, do something with the data.
-
+        // Give some alert messages if status is error
+        status = jsonObject.status;
+        if (status === "ERROR") {
+            alert(jsonObject.errorMessages);
+        } else {
+            // Change the questions paragraph content by adding the question from the server
+            question.innerHTML = jsonObject.questionText;
+            // Question attributes
+            let typeOfQuestion = jsonObject.questionType;
+            skipQuestion = jsonObject.canBeSkipped;
+            let requiresLocation = jsonObject.requiresLocation;
+            getTypeOfQuestion(typeOfQuestion);
+            getLocation(requiresLocation);
+            questionCanBeSkipped(skipQuestion);
+        }
     });
 }
