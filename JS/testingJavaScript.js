4cf06ceab4955916c694441dcbe570b2cb364d29
Kostas Ioannou <Kioannou2@uclan.ac.uk>
2019-12-01 11:07:01 +0200

    Testing page additions

--- JS/testingJavaScript.js
+++ JS/testingJavaScript.js
@@ -3,9 +3,11 @@
 const TESTING_API_LIST = "https://codecyprus.org/th/test-api/list?number-of-ths=2";
 
 
-
+function getTestingList(){
 fetch(TESTING_API_LIST)
     .then(response =>response.json()) //ParseJSON text to JavaScript object
     .then(jsonObject => {
         console.log(jsonObject); //TODO - Success, do something with the data.
+
     });
+}
