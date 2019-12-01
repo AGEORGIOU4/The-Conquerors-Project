46eabf37c4865f4b2fa7381d3bd424a7973e6407
AGEORGIOU4 <56922190+AGEORGIOU4@users.noreply.github.com>
2019-12-01 23:57:49 +0200

    Design

--- JS/javascript.js
+++ JS/javascript.js
@@ -9,6 +9,7 @@ const API_SKIP = "https://codecyprus.org/th/api/skip";
 // Parameters
 let sessionID = "";
 let uuid = "";
+let description = "";
 let appName = "";
 let skipQuestion = "";
 let latitude = "";
@@ -36,26 +37,28 @@ function getChallenges() {
                 // Traverse the array to get needed data
                 for (let i = 0; i < treasureHuntsArray.length; i++) {
                     // Get the Treasure Hunt uuid
+                    document.getElementById("treasureHuntsDescriptionParagraph").innerHTML = treasureHuntsArray[i].description;
                     let uuidLocal = treasureHuntsArray[i].uuid;
+                    let descriptionLocal = treasureHuntsArray[i].description;
                     // Create a button and style it for each TH challenge
-                    let treasureHuntsBtn = document.createElement('input');
-                    treasureHuntsBtn.type = "button";
-                    treasureHuntsBtn.value = treasureHuntsArray[i].name;
-                    treasureHuntsBtn.style.fontSize = "-webkit-xxx-large";
-                    treasureHuntsBtn.style.margin = "15px 25px";
-                    // The name of the button is the corresponding uuid
-                    treasureHuntsBtn.id = "treasureHuntsBtn" + [i + 1];
+                    let treasureHuntsButton = document.createElement('input');
+                    treasureHuntsButton.type = "button";
+                    treasureHuntsButton.value = treasureHuntsArray[i].name;
+                    treasureHuntsButton.style.fontSize = "-webkit-xxx-large";
+                    treasureHuntsButton.style.margin = "15px 25px";
+                    treasureHuntsButton.id = "treasureHuntsButton" + [i + 1];
+
+
                     // Define the uuid for each TH on click
-                    treasureHuntsBtn.onclick = function () {
-                        uuid = uuidLocal
+                    treasureHuntsButton.onclick = function () {
+                        uuid = uuidLocal;
+                        description = descriptionLocal;
+                        document.getElementById("treasureHuntsDescriptionParagraph").innerText = description;
                     };
                     /*--------------------------------CALL GET CREDENTIALS ON CLICK-----------------------------------*/
-                    treasureHuntsBtn.addEventListener("click", getCredentials);
-
+                    treasureHuntsButton.addEventListener("click", getCredentials);
                     // Add the TH challenges buttons on a list
-                    challengesList.appendChild(treasureHuntsBtn);
-
-                    /* List attributes */
+                    challengesList.appendChild(treasureHuntsButton);
 
                 }
             }
@@ -82,6 +85,7 @@ function hideChallenges() {
     selectTH.style.display = "block";
     selectTH2.style.display = "none";
 }
+
 /*--------------------------------------------------------------------------------------------------------------------*/
 
 // Call the first function to start the quiz
@@ -106,10 +110,19 @@ function getCredentials() {
     // Show username input
     let userInput = document.getElementById("usernameBox");
     userInput.style.display = "block";
+    document.getElementById("treasureHuntsDescriptionParagraph");
+    treasureHuntsDescriptionParagraph.style.display = "block";
+
+
 }
+
 /*--------------------------------------------------------------------------------------------------------------------*/
 
 function startSession(uuid) {
+    document.getElementById("loading");
+    loading.style.display = "block";
+    document.getElementById("treasureHuntsDescriptionParagraph");
+    treasureHuntsDescriptionParagraph.style.display = "none";
     // Get required parameters for START URL
     let playerName = document.getElementById("username").value;
     appName = "TheConquerors";
@@ -129,6 +142,8 @@ function startSession(uuid) {
                 alert(jsonObject.errorMessages);
             } else {
                 // If all params are correct (username, app name, session) call the questions and set (1)_(cookies!!!)
+                document.getElementById("loading");
+                loading.style.display = "none";
                 fetchQuestions(sessionID);
                 setCookies(sessionID);
             }
@@ -238,6 +253,7 @@ function getTypeOfQuestion(typeOfQuestion) {
         mcqButtons.style.display = "none";
     }
 }
+
 //==============================================================================================================//
 
 
@@ -252,24 +268,24 @@ function questionCanBeSkipped(skipQuestion) {
         skipButton.style.display = "block";
     }
 }
+
 //==============================================================================================================//
 function skipIt() {
-fetch(API_SKIP + "?session=" + sessionID)
-    .then(response => response.json()) //Parse JSON text to JavaScript object
-    .then(jsonObject => {
-        document.getElementById("skipButton");
-        console.log(jsonObject);
+    fetch(API_SKIP + "?session=" + sessionID)
+        .then(response => response.json()) //Parse JSON text to JavaScript object
+        .then(jsonObject => {
+            document.getElementById("skipButton");
+            console.log(jsonObject);
 
-        // Give some alert messages if the username is not valid
-        status = jsonObject.status;
-        if (status === "ERROR") {
-            alert(jsonObject.errorMessages);
-        } else {
+            // Give some alert messages if the username is not valid
+            status = jsonObject.status;
+            if (status === "ERROR") {
+                alert(jsonObject.errorMessages);
+            } else {
                 fetchQuestions(sessionID);
-        }
-    });
+            }
+        });
 }
-
 
 
 /*function getLeaderboard(sessionID) {
@@ -321,12 +337,12 @@ function setCookies(sessionID) {
 
 //=========================GET LOCATION=========================//
 function getLocation() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition);
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
 
-        } else {
-            alert("Geolocation is not supported by your browser.");
-        }
+    } else {
+        alert("Geolocation is not supported by your browser.");
+    }
 }
 
 function showPosition(position) {
@@ -350,67 +366,6 @@ function showPosition(position) {
 
 //=========================QR CODE READER=========================//
 function QRCodeReader() {
-    document.getElementById("preview");
-    preview.style.display = "block";
-
-    //opts
-    let opts = {
-        // Whether to scan continuously for QR codes. If false, use scanner.scan() to
-        // manually scan. If true, the scanner emits the "scan" event when a QR code is
-        // scanned. Default true.
-        continuous: true,
-
-        // The HTML element to use for the camera's video preview. Must be a <video>
-        // element. When the camera is active, this element will have the "active" CSS
-        // class, otherwise, it will have the "inactive" class. By default, an invisible
-        // element will be created to host the video.
-        video: document.getElementById('preview'),
-
-        // Whether to horizontally mirror the video preview. This is helpful when trying to
-        // scan a QR code with a user-facing camera. Default true.
-        mirror: false,
-
-        // Whether to include the scanned image data as part of the scan result. See the
-        // "scan" event for image format details. Default false.
-        captureImage: false,
-
-        // Only applies to continuous mode. Whether to actively scan when the tab is not
-        // active.
-        // When false, this reduces CPU usage when the tab is not active. Default true.
-        backgroundScan: true,
-
-        // Only applies to continuous mode. The period, in milliseconds, before the same QR
-        // code will be recognized in succession. Default 5000 (5 seconds).
-        refractoryPeriod: 5000,
-
-        // Only applies to continuous mode. The period, in rendered frames, between scans. A
-        // lower scan period increases CPU usage but makes scan response faster.
-        // Default 1 (i.e. analyze every frame).
-        scanPeriod: 1
-    };
-
-
-    let scanner = new Instascan.Scanner(opts);
-
-    Instascan.Camera.getCameras().then(function (cameras) {
-        if (cameras.length > 0) {
-            scanner.start(cameras[0]);
-        } else {
-            console.error('No cameras found.');
-            alert("No cameras found.");
-        }
-    }).catch(function (e) {
-        console.error(e);
-    });
-
-    scanner.addListener('scan', function (content) {
-        console.log(content);
-        document.getElementById("content").innerHTML = content;
-    });
-}
-
-//=========================QR CODE READER=========================//
-function QRCodeReader2() {
     document.getElementById("preview");
     preview.style.display = "block";
 
