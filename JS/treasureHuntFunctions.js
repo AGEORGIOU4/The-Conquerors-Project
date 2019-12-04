bdc95cd9f9a5177f858c50828826e8da2aa4977d
andreasgeorgiou <ageorgiou4@uclan.ac.uk>
2019-12-04 17:03:39 +0200

    ....

--- JS/treasureHuntFunctions.js
+++ JS/treasureHuntFunctions.js
@@ -20,6 +20,8 @@ let longitude = "";
 const cookieSession = "cookieSession";
 const cookiePlayerName = "cookiePlayerName";
 
+let cookies = document.cookie;
+
 /*--------------------------------------------------------------------------------------------------------------------*/
 
 // Get Challenges
@@ -135,14 +137,14 @@ function startSession() {
                 // If all params are correct (username, app name, session) call the questions
                 document.getElementById("treasureHuntsDescriptionParagraph").style.display = "none";
                 document.getElementById("loading").style.display = "none";
+                console.log(cookies);
                 fetchQuestions(sessionID);
             }
         });
 }
 
 function resumeSession() {
-playerName = cookiePlayerName;
-   startSession(cookieSession,cookiePlayerName);
+   getCookie(cookieSession);
 
 }
 
@@ -315,6 +317,22 @@ function setCookie(cname, cvalue, exdays) {
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+ d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
 
 
